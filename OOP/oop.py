01299e5754c524276668cc52a9f246e142786ecc
Ruslan Trifonov <ruslan.trifonov@1win.pro>
2025-01-10 19:41:49 +0500

    OOP

--- OOP/oop.py
+++ OOP/oop.py
@@ -1,3 +1,12 @@
+"""
+Class: template for creating objects. All objects created using the same class will have the same characteristics.
+Object: an instance of a class.
+Instantiate: create an instance of a class.
+Method: a function defined in a class.
+Attribute: a variable bound to an instance of a class.
+"""
+
+
 class Kettle(object):
 
     def __init__(self, make, price):
@@ -15,3 +24,5 @@ print(kenwood.price)
 hamilton = Kettle('Hamilton', 333)
 
 print(f"Models: {kenwood.make} = {kenwood.price}, {hamilton.make} = {hamilton.price}")
+
+print(type(kenwood))
